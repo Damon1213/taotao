3e80aef02bcf530d962cb759e65fa3e9a738f213
hu <hudongdong@voicecodes.com>
2018-05-29 17:29:36 +0800

    增加门户网页商品列表展示

---
+++ taotao-rest/src/main/java/com/taotao/rest/pojo/CatResult.java
@@ -0,0 +1,19 @@
+package com.taotao.rest.pojo;
+
+import java.util.List;
+
+/**
+ * Created by hu on 2018-05-29.
+ */
+public class CatResult {
+
+    private List<?> data;
+
+    public List<?> getData() {
+        return data;
+    }
+
+    public void setData(List<?> data) {
+        this.data = data;
+    }
+}
---
+++ taotao-rest/src/main/java/com/taotao/rest/service/ItemCatService.java
@@ -0,0 +1,10 @@
+package com.taotao.rest.service;
+
+import com.taotao.rest.pojo.CatResult;
+
+/**
+ * Created by hu on 2018-05-29.
+ */
+public interface ItemCatService {
+    CatResult getItemCatList();
+}
