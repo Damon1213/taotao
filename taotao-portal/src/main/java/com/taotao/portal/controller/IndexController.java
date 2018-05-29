dae2066d5f10d7441aa3378fa3e4e918fc45ef8f
hu <hudongdong@voicecodes.com>
2018-05-29 09:00:53 +0800

    遗漏pom

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by hu on 2018-05-28.
