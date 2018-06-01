b0b91ea38ca81e64fb200757b0505df31b85b992
hu <hudongdong@voicecodes.com>
2018-06-01 13:56:02 +0800

    门户网站商品分类实现redis缓存（）

--- taotao-rest/src/main/java/com/taotao/rest/service/impl/ItemCatServiceImpl.java
+++ taotao-rest/src/main/java/com/taotao/rest/service/impl/ItemCatServiceImpl.java
@@ -1,12 +1,16 @@
 package com.taotao.rest.service.impl;
 
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.mapper.TbItemCatMapper;
 import com.taotao.pojo.TbItemCat;
 import com.taotao.pojo.TbItemCatExample;
+import com.taotao.rest.dao.JedisClient;
 import com.taotao.rest.pojo.CatNode;
 import com.taotao.rest.pojo.CatResult;
 import com.taotao.rest.service.ItemCatService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,12 +24,40 @@ public class ItemCatServiceImpl implements ItemCatService {
 
     @Autowired
     private TbItemCatMapper itemCatMapper;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${INDEX_ITEM_CAT_REDIS_KEY}")
+    private String INDEX_ITEM_CAT_REDIS_KEY;
 
     @Override
     public CatResult getItemCatList() {
         CatResult catResult = new CatResult();
+        //从缓存中取
+        try {
+            String result = jedisClient.hget(INDEX_ITEM_CAT_REDIS_KEY, 0 + "");
+            //判断字符串是否为空
+            if (!StringUtils.isBlank(result)){
+                //字符串转换成pojo
+                CatResult pojo = JsonUtils.jsonToPojo(result, CatResult.class);
+                return pojo;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
         //查询分类列表
-        catResult.setData(getCatList(0));
+        List<?> list = getCatList(0);
+        catResult.setData(list);
+
+        //写入缓存
+        try {
+            //把list转换成字符串
+            String json = JsonUtils.objectToJson(catResult);
+            jedisClient.hset(INDEX_ITEM_CAT_REDIS_KEY,0 + "",json);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         return catResult;
     }
