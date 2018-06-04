7fbfd20889dc722c31363d0b81f70d008cc0b9f3
hu <hudongdong@voicecodes.com>
2018-06-04 10:32:33 +0800

    商品数据导入solr

---
+++ taotao-search/src/main/java/com/taotao/search/mapper/ItemMapper.java
@@ -0,0 +1,13 @@
+package com.taotao.search.mapper;
+
+import com.taotao.search.pojo.Item;
+
+import java.util.List;
+
+/**
+ * Created by hu on 2018-06-01.
+ */
+public interface ItemMapper {
+
+    List<Item> getItemList();
+}
---
+++ taotao-search/src/main/java/com/taotao/search/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.search.service;
+
+import com.taotao.common.utils.TaotaoResult;
+
+/**
+ * Created by hu on 2018-06-01.
+ */
+public interface ItemService {
+    TaotaoResult importAllItems();
+}
